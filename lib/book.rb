fc2312c5b4f1562ef7ce0197352a0d2a0a758816
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-21 17:23:35 -0400

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
-
+class Book
+  attr_reader :title
+  # attr_writer :page_count
+  attr_accessor :author, :page_count, :genre
+  def initialize title
+    @title = title
+    @author = author
+  end
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+  attr_reader :brand
+  # attr_writer :color
+  attr_accessor :color, :size, :material, :condition
+  def initialize brand
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+  def cobble
+    puts 'Your shoe is as good as new!'
+    self.condition = 'new'
+  end
+end
